904fcf466b1d65530a245be6c08004aade280602
benoitc <bchesneau@gmail.com>
2011-06-22 08:43:50 +0200

    use last icu4c version and reduce 50% of the size in .so by using only collations data.

--- utils/fetch_libs.sh
+++ utils/fetch_libs.sh
@@ -2,10 +2,18 @@
 
 CURL_BIN=`which curl`
 
-ICU_URI="http://rcouch.refuge.io/dl/libs/icu4c-4_6_1-src.tgz"
-TARGET=`pwd`/c_src/icu4c-4_6_1-src.tgz
+ICU_URI="http://rcouch.refuge.io/dl/libs/icu4c-4_8-src.tgz"
+ICUDATA_URI="http://rcouch.refuge.io/dl/libs/icudt48l.zip"
+TARGET=`pwd`/c_src/icu4c-4_8-src.tgz
+TARGET1=`pwd`/c_src/icudt48l.zip
+
 
 if ! test -f $TARGET; then
-    echo "==> Fetch ${TARGET}"
+    echo "==> Fetch icu library"
     $CURL_BIN $ICU_URI -o $TARGET
+fi
+
+if ! test -f $TARGET1; then
+    echo "==> Fetch icu data"
+    $CURL_BIN $ICUDATA_URI -o $TARGET1
 fi
